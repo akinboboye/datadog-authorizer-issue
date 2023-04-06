89f7363d2230d47d5cfd1940f2f5b9a86b83c22a
Hotel Engine <hotelengine@Shinas-MBP.attlocal.net>
2023-04-06 13:05:56 -0400

    updated authorizer to use callback style

--- src/authorizer.ts
+++ src/authorizer.ts
@@ -1,29 +1,32 @@
 import {
   APIGatewayRequestAuthorizerEvent,
-  APIGatewayAuthorizerResult
+  Context,
+  Callback
 } from 'aws-lambda';
 import { datadog } from 'datadog-lambda-js';
 
 const authorizer = async (
-  event: APIGatewayRequestAuthorizerEvent
-): Promise<APIGatewayAuthorizerResult> => {
+  event: APIGatewayRequestAuthorizerEvent,
+  _: Context,
+  callback: Callback
+): Promise<void> => {
   const shouldAuthorizeRequest =
     process.env.SHOULD_AUTHORIZE_REQUEST?.toLowerCase() === 'true'
 
   if (!shouldAuthorizeRequest) {
-    throw new Error("Unauthorized");
-  }
-
-  return {
-    policyDocument: {
-      Version: '2012-10-17',
-      Statement: [{
-        Action: 'execute-api:Invoke',
-        Effect: 'Allow',
-        Resource: '*'
-      }],
-    },
-    principalId: 'user'
+    callback('Unauthorized')
+  } else {
+    callback(null, {
+      policyDocument: {
+        Version: '2012-10-17',
+        Statement: [{
+          Action: 'execute-api:Invoke',
+          Effect: 'Allow',
+          Resource: '*'
+        }],
+      },
+      principalId: 'user'
+    })
   }
 };
 
